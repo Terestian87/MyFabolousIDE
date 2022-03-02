56bd9fac2a91975caf117c85737d355e7bb2022c
Claudio Flocco <claudio.flocco@yahoo.co.uk>
2022-03-02 22:09:36 +0100

    removed redux config and unnecessary dependencies

--- src/components/app/rootreducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit'
-import { store } from './store'
-import mainSlice from '../features/mainSlice'
-
-export const rootReuducer = combineReducers({
-    mainState: mainSlice
-})
-
-export type RootState = ReturnType<typeof store.getState>
