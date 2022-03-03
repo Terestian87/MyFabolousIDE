faa7a10ea0c48587e285091e6cdda9df60d86a19
Claudio Flocco <claudio.flocco@yahoo.co.uk>
2022-03-03 10:12:24 +0100

    fix(index): removed redux traces

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { store } from "./components/app/store";
-import { Provider } from "react-redux";
 import App from "./components/App";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-)
+ReactDOM.render(<App />, document.getElementById("root"))
